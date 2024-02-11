196effce7ab2d5336c1790c2b3af176f1949eea6
Kai <136880922+euiyle@users.noreply.github.com>
2024-02-12 03:24:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '6cd6c2f4741ee4cb5ee315fcb94b56ac', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
