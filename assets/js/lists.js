44f7967948cece8c6bbefa5d8d0ebf19472b4369
Kai <136880922+euiyle@users.noreply.github.com>
2024-02-12 03:50:05 +0800

    Update lists.js

--- assets/js/lists.js
+++ assets/js/lists.js
@@ -18,16 +18,28 @@ const generateFirstListsContainer = () => {
           href="${list.links[1].link}"
           class="listItem"
           >${list.links[1].name}</a>
-	  <a
-          target="${CONFIG.openInNewTab ? '_blank' : ''}"
-          href="${list.links[2].link}"
-          class="listItem"
-          >${list.links[2].name}</a>
+        </div>
+      `;
+		const position = 'beforeend';
+		lists_1.insertAdjacentHTML(position, item);
+	}
+};
+
+const generateSecondListsContainer = () => {
+	for (const list of CONFIG.secondlistsContainer) {
+		let item = `
+        <div class="card list list__${list.id}" id="list_${list.id}">
+          <i class="listIcon" icon-name="${list.icon}"></i>
           <a
           target="${CONFIG.openInNewTab ? '_blank' : ''}"
-          href="${list.links[3].link}"
+          href="${list.links[0].link}"
           class="listItem"
-          >${list.links[3].name}</a>
+          >${list.links[0].name}</a>
+          <a
+          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          href="${list.links[1].link}"
+          class="listItem"
+          >${list.links[1].name}</a>
         </div>
       `;
 		const position = 'beforeend';
@@ -42,6 +54,7 @@ const generateLists = () => {
 			break;
 		case 'lists':
 			generateFirstListsContainer();
+			generateSecondListsContainer();
 			break;
 		default:
 			break;
