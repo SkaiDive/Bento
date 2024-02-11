1fa68f7b130dd84dba18e73a734ddb85c424fce3
Kai <136880922+euiyle@users.noreply.github.com>
2024-02-12 02:24:44 +0800

    meow

--- config.js
+++ config.js
@@ -88,7 +88,7 @@ const CONFIG = {
 			link: 'https://instagram.com/',
 		},
 	],
-
+	
 	// ┬  ┬┌─┐┌┬┐┌─┐
 	// │  │└─┐ │ └─┐
 	// ┴─┘┴└─┘ ┴ └─┘
@@ -106,53 +106,6 @@ const CONFIG = {
 				{
 					name: 'bitchin'',
 					link: 'https://open.spotify.com/playlist/5YHLdrygNyLTguirxuYJ4i',
-				},
-			],
-		},
-	],
-		// Second Links Container
-	secondListsContainer: [
-		{
-			icon: 'binary',
-			id: '1',
-			links: [
-				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
-				},
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
-				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
-				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
-				},
-				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
-				},
-				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
 				},
 			],
 		},
